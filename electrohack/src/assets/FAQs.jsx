4e8e8048c69801783fa9869d134d45b9ba731351
Das2225 <suhani22leo@gmail.com>
2025-01-24 17:31:51 +0530

    added text

--- electrohack/src/assets/FAQs.jsx
+++ electrohack/src/assets/FAQs.jsx
@@ -14,41 +14,36 @@ export default function FAQs(){
                 <div style={{width:"50vw"}}>
                 <Accordion type="single"  collapsible className="w-full bg-black text-white mt-12">
       <AccordionItem value="item-1" className="border-gray-500">
-        <AccordionTrigger  style={{fontSize:"1.5vw"}}>Is it accessible?</AccordionTrigger>
-        <AccordionContent className=" font-light" style={{fontSize:"1.2vw"}}>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
+        <AccordionTrigger  style={{fontSize:"1.5vw"}}>How many people can register in a team ?</AccordionTrigger>
+        <AccordionContent className=" font-light" style={{fontSize:"1.2vw"}}>3-5 students can be registered in a team.</AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2" className="border-gray-500">
-        <AccordionTrigger style={{fontSize:"1.5vw"}}>Is it styled?</AccordionTrigger>
+        <AccordionTrigger style={{fontSize:"1.5vw"}}>What should I do if I don't have a team?</AccordionTrigger>
         <AccordionContent className=" font-light" style={{fontSize:"1.2vw"}}>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
+        No problem, we will arrange a team for you.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3" className="border-gray-500"> 
-        <AccordionTrigger style={{fontSize:"1.5vw"}}>Is it animated?</AccordionTrigger>
+        <AccordionTrigger style={{fontSize:"1.5vw"}}>Should we get our own hardware components?</AccordionTrigger>
         <AccordionContent className=" font-light" style={{fontSize:"1.2vw"}}>
-          Yes. It's animated by default, but you can disable it if you prefer.
+        It's recommended to get your own components, but nevertheless we'll also provide the components available in the inventory list
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-4" className="border-gray-500">
-        <AccordionTrigger style={{fontSize:"1.5vw"}}>Is it accessible?</AccordionTrigger>
+        <AccordionTrigger style={{fontSize:"1.5vw"}}>Will we be allowed to leave the venue during the night time?</AccordionTrigger>
         <AccordionContent className=" font-light" style={{fontSize:"1.2vw"}}>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
+        Only to use the restrooms, and your ID card will be taken for the time period, but you won't be allowed to go to your room until 6:00 am.</AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-5" className="border-gray-500 ">
-        <AccordionTrigger style={{fontSize:"1.5vw"}}>Is it styled?</AccordionTrigger>
+        <AccordionTrigger style={{fontSize:"1.5vw"}}>Is there any entry fee or cash prize involved?</AccordionTrigger>
         <AccordionContent className=" font-light" style={{fontSize:"1.2vw"}}>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
+          The participation is free, whereas there are cash prizes for the winners and certificates for all participants.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-6" className="border-gray-500"> 
-        <AccordionTrigger style={{fontSize:"1.5vw"}}>Is it animated?</AccordionTrigger>
+        <AccordionTrigger style={{fontSize:"1.5vw"}}>Are there any prerequisites for the event?</AccordionTrigger>
         <AccordionContent className=" font-light" style={{fontSize:"1.2vw"}}>
-          Yes. It's animated by default, but you can disable it if you prefer.
+        Yes, hunger to learn and innovate.
         </AccordionContent>
       </AccordionItem>
     </Accordion>
